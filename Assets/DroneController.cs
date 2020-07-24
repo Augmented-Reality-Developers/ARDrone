af75b006fcbaf61029c7fffcedb736842a5a46e1
Qusai Azzam <eng.qusai.azzam@gmail.com>
2020-07-24 15:53:15 +0400

    initial commit

---
+++ Assets/DroneController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    public enum DroneState {
+        DRONE_STATE_IDLE,
+        DRONE_STATE_START_TAKINGOFF,
+        DRONE_STATE_TAKINGOFF,
+        DRONE_MOVING_UP,
+        DRONE_FLYING,
+        DRONE_STATE_START_LANGIN,
+        DRONE_STATE_LANDING,
+        DRONE_STATE_LANDED,
+        DRONE_STATE_WAIT_ENDING_STOP,
+    }
+
+    DroneState _state;
+
+    Animator _anim;
+    Vector3 _speed = new Vector3(0f,0f,0f);
+    
+    public float speed_multiplayer;
+
+    public bool IsIdle() {
+        return (_state == DroneState.DRONE_STATE_IDLE);
+    }
+
+    public bool IsFlying()
+    {
+        return (_state == DroneState.DRONE_FLYING);
+    }
+    public void TakeOff()
+    {
+        _state = DroneState.DRONE_STATE_START_TAKINGOFF;
+    }
+    public void Land()
+    {
+        _state = DroneState.DRONE_STATE_START_LANGIN;
+    }
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+
+    // Move the AirDrone
+    public void Move(float speed_x, float speed_z)
+    {
+        _speed.x = speed_x;
+        _speed.z = speed_z;
+        UpdateDronePosition();
+    }
+
+    void UpdateDronePosition()
+    {
+        switch (_state)
+        {
+            case DroneState.DRONE_STATE_IDLE:
+                break;
+
+            case DroneState.DRONE_STATE_START_TAKINGOFF:
+                _anim.SetBool("TakeOff", true);
+                _state = DroneState.DRONE_STATE_TAKINGOFF;
+                break;
+
+            case DroneState.DRONE_STATE_TAKINGOFF:
+                if (_anim.GetBool("TakeOff") == false) {
+                    _state = DroneState.DRONE_MOVING_UP;
+                }
+                break;
+
+            case DroneState.DRONE_MOVING_UP:
+                if (_anim.GetBool("MoveUp") == false) {
+                    _state = DroneState.DRONE_FLYING;
+                }
+                break;
+
+            case DroneState.DRONE_FLYING:
+                float deltaTime = Time.deltaTime;
+                float angle_Z = -30.0f * _speed.x * 60.0f * deltaTime;
+                float angle_X = 30.0f * _speed.z * 60.0f * deltaTime;
+
+                Vector3 rotation = transform.eulerAngles;
+                transform.localPosition += _speed * speed_multiplayer * deltaTime;
+                transform.localRotation = Quaternion.Euler(angle_X, rotation.y, angle_Z);
+                break;
+
+            case DroneState.DRONE_STATE_START_LANGIN:
+                _anim.SetBool("MoveDown",true);
+                _state = DroneState.DRONE_STATE_LANDING;
+                break;
+
+
+            case DroneState.DRONE_STATE_LANDING:
+                if (_anim.GetBool("MoveDown") == false)
+                {
+                    _state = DroneState.DRONE_STATE_LANDED;
+                }
+                break;
+
+
+            case DroneState.DRONE_STATE_LANDED:
+                _anim.SetBool("Land", true);
+                _state = DroneState.DRONE_STATE_WAIT_ENDING_STOP;
+                break;
+
+            case DroneState.DRONE_STATE_WAIT_ENDING_STOP:
+                if (_anim.GetBool("Land") == false)
+                {
+                    _state = DroneState.DRONE_STATE_IDLE;
+                }
+                break;
+        }        
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public DroneController _drone;
+    public Button flyButton;
+    public Button landButton;
+
+    // Moving Controls
+    public GameObject controls;
+
+
+    // AR
+    public ARRaycastManager rayManager;
+    public ARPlaneManager planeManager;
+    List<ARRaycastHit> hitResult = new List<ARRaycastHit>();
+
+
+    // Drone 
+    public GameObject _Drone;
+
+
+    struct DroneAnimationControls {
+        public bool _moving;
+        public bool _interpolatingAsc;
+        public bool _interpolatingDsc;
+        public float _axis;
+        public float _direction;
+    }
+
+    DroneAnimationControls _movingLeft;
+    DroneAnimationControls _movingBack;
+
+    void Start() {
+        flyButton.onClick.AddListener(OnClickFlyButton);
+        landButton.onClick.AddListener(OnClickLandButton);
+    }
+
+    void UpdateDroneControls(ref DroneAnimationControls control)
+    {
+        if (control._moving || control._interpolatingAsc || control._interpolatingDsc) {
+            if (control._interpolatingAsc)
+            {
+                control._axis += 0.5f;
+                if (control._axis > 1.0f) {
+                    control._interpolatingAsc = false;
+                    control._interpolatingDsc = true;
+                    control._axis = 1.0f;
+                }
+            }
+            else if (!control._moving)
+            {
+                control._axis -= 0.5f;
+                if (control._axis < 0.0f)
+                {
+                    control._axis = 0.0f;
+                    control._interpolatingDsc = false;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()  {
+        
+        float x_speed = Input.GetAxis("Horizontal");
+        float z_speed = Input.GetAxis("Vertical");
+      
+        UpdateDroneControls(ref _movingLeft);
+        UpdateDroneControls(ref _movingBack);
+
+        _drone.Move(_movingLeft._axis * _movingLeft._direction , _movingBack._axis * _movingBack._direction);
+
+        if (_drone.IsIdle())
+        {
+            UpdateAR();
+        }
+    }
+
+    public void OnClickFlyButton() {
+        if (_drone.IsIdle()) {
+            _drone.TakeOff();
+            flyButton.gameObject.SetActive(false);
+            controls.SetActive(true);
+        }
+    }
+
+    public void OnClickLandButton() {
+        if (_drone.IsFlying()) {
+            _drone.Land();
+            flyButton.gameObject.SetActive(true);
+            controls.SetActive(false);
+        }
+    }
+    public void OnClickLeftButton() {
+        _movingLeft._moving = true;
+        _movingLeft._interpolatingAsc = true;
+        _movingLeft._direction = -1.0f;
+    }
+
+    public void OnReleasedLeftButton() {
+        _movingLeft._moving = false;
+    }
+
+    public void OnClickRightButton() {
+        _movingLeft._moving = true;
+        _movingLeft._interpolatingAsc = true;
+        _movingLeft._direction = 1.0f;
+    }
+    public void OnReleaseRightButton() {
+        _movingLeft._moving = false;
+    }
+
+    public void OnClickBackButton() {
+        _movingBack._moving = true;
+        _movingBack._interpolatingAsc = true;
+        _movingBack._direction = -1.0f;
+    }
+
+    public void OnReleaseBackButton()
+    {
+        _movingBack._moving = false;
+    }
+
+    public void OnClickForwardButton()
+    {
+        _movingBack._moving = true;
+        _movingBack._interpolatingAsc = true;
+        _movingBack._direction = 1.0f;
+    }
+    public void OnReleaseForwardButton() {
+        _movingBack._moving = false;
+    }
+
+
+    public void UpdateAR() {
+        Vector2 positionScreen = Camera.main.ViewportToScreenPoint(new Vector2(0.5f,0.5f));
+        rayManager.Raycast(positionScreen, hitResult, TrackableType.PlaneWithinBounds);
+        if (hitResult.Count > 0) {
+            if (planeManager.GetPlane(hitResult[0].trackableId).alignment == PlaneAlignment.HorizontalUp)
+            {
+                Pose pose = hitResult[0].pose;
+                _Drone.transform.position = pose.position;
+                _Drone.SetActive(true);
+            }
+        }
+    }
+}
